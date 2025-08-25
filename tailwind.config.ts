c028214c99bc3424e01d0155ce6d4776f1dd269a
llmojoll <wnsgusqkqh21@gmail.com>
2025-08-26 03:48:07 +0900

    Feat : tailwind.config 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,32 @@
+// tailwind.config.ts
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
   theme: {
     extend: {
+      // 폰트
+      fontFamily: {
+        sans: ['var(--font-pretendard)', 'sans-serif'], // 기본
+        iropke: ['var(--font-iropke)', 'serif'],
+      },
+      fontSize: {
+        '3xl': ['32px', '42px'],
+        '2xl': ['24px', '32px'],
+        xl: ['20px', '32px'],
+        '2lg': ['18px', '26px'],
+        lg: ['16px', '26px'],
+        md: ['14px', '24px'],
+        sm: ['13px', '22px'],
+        xs: ['12px', '18px'],
+      },
+      fontWeight: {
+        regular: '400',
+        medium: '500',
+        semibold: '600',
+        bold: '700',
+      },
+
+      // 색상
       colors: {
         black: {
           100: '#F9F9F9',
