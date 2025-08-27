544967742cbddeaf835ecfade35a4a02c2477a35
llmojoll <wnsgusqkqh21@gmail.com>
2025-08-27 22:55:18 +0900

    Feat : navbar에 목록 아이콘, 유저 아이콘 추가 , navbar 반응형 추가

--- src/components/common/Gnb.tsx
+++ src/components/common/Gnb.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import GnbMenu from '@/assets/gnb-menu.svg';
+import UserIcon from '@/assets/user.svg';
 import Logo from '@/components/common/Logo';
 
 export default function Gnb() {
@@ -7,17 +9,21 @@ export default function Gnb() {
     // <nav className='flex items-center justify-center h-16 px-6 border-b border-gray-200 bg-white'>
     //   <Logo />
     // </nav>
-    <nav className='flex items-center h-16 px-6 border-b border-gray-200 bg-white'>
+    <nav className='flex items-center h-[52px] md:h-[60px] lg:h-20 px-6 md:px-[72px] lg:px-[120px] border-b border-gray-200 bg-white'>
       {/* 왼쪽 그룹 */}
-      <div className='flex items-center gap-4'>
+      <div className='flex items-center'>
+        <GnbMenu className='w-[24px] h-[24px] mr-[12px]' />
         <Logo />
-        <button className='px-3 py-2 rounded-lg text-sm font-medium'>Feed</button>
-        <button className='px-3 py-2 rounded-lg text-sm font-medium'>검색</button>
+        <button className='ml-[24px] lg:ml-[36px] mr-[24px] text-md font-semibold'>피드</button>
+        <button className='text-md font-semibold'>검색</button>
       </div>
 
       {/* 오른쪽 그룹 */}
       <div className='flex items-center ml-auto'>
-        <button className='px-3 py-2 rounded-lg text-sm font-medium text-gray-300'>김코드</button>
+        <button className='flex items-center'>
+          <UserIcon className='w-4 lg:w-6 h-4 lg:h-6 mr-[6px] text-gray-300' />
+          <p className='text-sm lg:text-md font-medium text-gray-300'>김코드</p>
+        </button>
       </div>
     </nav>
   );
