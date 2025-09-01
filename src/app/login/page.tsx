1e929c6e8fff84da58e34eac9561f6801405a9bb
llmojoll <wnsgusqkqh21@gmail.com>
2025-09-01 23:04:40 +0900

    Feat : 로그인페이지 레이아웃 구현

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,50 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import Logo from '@/components/common/Logo';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+
 export default function Login() {
-  return <div className='bg-black-100 w-[300px] h-[200px]'>로그인 페이지</div>;
+  return (
+    <div className='flex justify-center items-center flex-col w-[312px] md:w-[384px] lg:w-[640px] mt-[128px] md:mt-[100px] lg:mt-[213px] m-auto'>
+      <div>
+        <Logo />
+      </div>
+
+      <div className='w-full mt-[50px] md:mt-[60px]'>
+        <form>
+          <Input placeholder='이메일' />
+          <Input placeholder='비밀번호' className='mt-2.5 lg:mt-4' />
+          <Button variant='black500' size='lg' type='submit' className='mt-5 lg:mt-6'>
+            로그인
+          </Button>
+        </form>
+      </div>
+
+      <div className='w-full flex justify-end mt-2.5 text-md md:text-lg lg:text-xl font-medium'>
+        <p className='mr-2 text-blue-400'>회원이 아니신가요?</p>
+        <Link href='/signup' className='underline'>
+          가입하기
+        </Link>
+      </div>
+
+      <div className='w-full flex items-center mt-[50px] md:mt-[60px]'>
+        <hr className='flex-1 border-blue-400' />
+        <span className='mx-2 text-blue-400 text-xs lg:text-xl font-regular'>
+          SNS 계정으로 로그인하기
+        </span>
+        <hr className='flex-1 border-blue-400' />
+      </div>
+
+      <div className='w-full flex justify-center gap-4 mt-6 lg:mt-10'>
+        <Button className='p-[11px] lg:p-4' size='icon'>
+          <Image src='/googlelogo.png' width={60} height={60} alt='구글 로고 이미지' />
+        </Button>
+        <Button className='p-[11px] lg:p-4' size='icon'>
+          <Image src='/kakaologo.png' width={60} height={60} alt='카카오 로고 이미지' />
+        </Button>
+      </div>
+    </div>
+  );
 }
