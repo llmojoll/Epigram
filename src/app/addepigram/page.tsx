b2b811cdc357a81381651347b1fdc33a2bf8df93
llmojoll <wnsgusqkqh21@gmail.com>
2025-09-05 01:38:51 +0900

    Feat : addepigram 페이지 ui 및 레이아웃 구현

--- src/app/addepigram/page.tsx
+++ src/app/addepigram/page.tsx
@@ -1,3 +1,47 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
+
 export default function AddEpigram() {
-  return <div className='bg-black-100 w-[300px] h-[200px]'>에피그램 추가 페이지</div>;
+  return (
+    <div className='flex justify-center items-start flex-col m-auto w-[312px] md:w-[384px] lg:w-[640px] mt-[24px] md:mt-[32px] lg:mt-[56px] mb-[30px] '>
+      <h3 className='text-lg md:text-xl lg:text-2xl font-semibold'>에피그램 만들기</h3>
+      <div className='w-[312px] md:w-[384px] lg:w-[640px] mt-6 md:mt-8 lg:mt-10'>
+        <p className='flex items-center text-md md:text-lg lg:text-xl font-semibold'>
+          내용 <span className='text-red-500 ml-1'>*</span>
+        </p>
+        <Textarea
+          placeholder='500자 이내로 입력해주세요.'
+          className='w-full resize-none mt-2 lg:mt-6 h-[132px] lg:h-[148px]'
+        />
+
+        <p className='flex items-center text-md md:text-lg lg:text-xl font-semibold mt-10 lg:mt-[54px]'>
+          저자 <span className='text-red-500 ml-1'>*</span>
+        </p>
+        <p className='mt-2 lg:mt-6'>라디오버튼</p>
+        <Input placeholder='저자 이름 입력' className='bg-transparent h-11 lg:h-16 mt-3 lg:mt-4' />
+
+        <p className='flex items-center text-md md:text-lg lg:text-xl font-semibold mt-10 lg:mt-[54px]'>
+          출처
+        </p>
+        <Input placeholder='출처 제목 입력' className='bg-transparent h-11 lg:h-16 mt-3 lg:mt-4' />
+        <Input
+          placeholder='URL (ex. https://www.website.com)'
+          className='bg-transparent h-11 lg:h-16 mt-3 lg:mt-4'
+        />
+
+        <p className='flex items-center text-md md:text-lg lg:text-xl font-semibold mt-10 lg:mt-[54px]'>
+          태그
+        </p>
+        <Input
+          placeholder='입력하여 태그 작성 (최대 10자)'
+          className='bg-transparent h-11 lg:h-16 mt-3 lg:mt-4'
+        />
+
+        <Button variant='black500' className='mt-6 lg:mt-10 text-lg lg:text-xl font-semibold'>
+          작성 완료
+        </Button>
+      </div>
+    </div>
+  );
 }
