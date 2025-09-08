1d6cbe5cb7d4af0038d42370b2d5a656506f7225
llmojoll <wnsgusqkqh21@gmail.com>
2025-09-08 15:58:11 +0900

    Feat : 에피그렘리스트 페이지 경로 추가

--- src/app/epigramlist/page.tsx
+++ src/app/epigramlist/page.tsx
@@ -1,8 +1,13 @@
+'use client';
+
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 import { Button } from '@/components/ui/button';
 
 export default function EpigramList() {
+  const router = useRouter();
+
   return (
     <div className='flex justify-center items-center flex-col m-auto w-[312px] md:w-[600px] lg:w-[1200px] mt-[32px] lg:mt-[120px] m-auto mb-[56px] '>
       <header className='flex justify-start w-full'>
@@ -21,12 +26,13 @@ export default function EpigramList() {
           </Button>
         </div>
         <div className='fixed flex items-end flex-col bottom-10 right-10 '>
-          <Button variant='blue900' size='md'>
+          <Button variant='blue900' size='md' onClick={() => router.push('/addepigram')}>
             + 에피그램 만들기
           </Button>
           <Button
             variant='blue900'
             className='w-[48px] lg:w-[64px] h-[48px] lg:h-[64px] rounded-full text-white mt-2'
+            onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
           >
             <Image src='/pageupbtn.svg' width={20} height={20} alt='페이지 최상단으로 가는 버튼' />
           </Button>
