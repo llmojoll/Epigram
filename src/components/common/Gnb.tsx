81e52fe4bfcc7ba9a50b878bb1635d4f5b3e9a4f
llmojoll <wnsgusqkqh21@gmail.com>
2025-09-13 02:20:51 +0900

    Feat : Gnb 경로 설정

--- src/components/common/Gnb.tsx
+++ src/components/common/Gnb.tsx
@@ -1,10 +1,15 @@
 'use client';
 
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+
 import GnbMenu from '@/assets/gnb-menu.svg';
 import UserIcon from '@/assets/user.svg';
 import Logo from '@/components/common/Logo';
 
 export default function Gnb() {
+  const router = useRouter();
+
   return (
     // <nav className='flex items-center justify-center h-16 px-6 border-b border-gray-200 bg-white'>
     //   <Logo />
@@ -13,14 +18,24 @@ export default function Gnb() {
       {/* 왼쪽 그룹 */}
       <div className='flex items-center'>
         <GnbMenu className='w-[24px] h-[24px] mr-[12px]' />
-        <Logo />
-        <button className='ml-[24px] lg:ml-[36px] mr-[24px] text-md font-semibold'>피드</button>
+
+        <Link href='/'>
+          <Logo />
+        </Link>
+
+        <Link
+          href='/epigramlist'
+          className='ml-[24px] lg:ml-[36px] mr-[24px] text-md font-semibold'
+        >
+          피드
+        </Link>
+
         <button className='text-md font-semibold'>검색</button>
       </div>
 
       {/* 오른쪽 그룹 */}
       <div className='flex items-center ml-auto'>
-        <button className='flex items-center'>
+        <button className='flex items-center' onClick={() => router.push('/login')}>
           <UserIcon className='w-4 lg:w-6 h-4 lg:h-6 mr-[6px] text-gray-300' />
           <p className='text-sm lg:text-md font-medium text-gray-300'>김코드</p>
         </button>
